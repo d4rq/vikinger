6a9db2bde2a45da054d9799fb1be2ff2bc85cdc5
Nikolai Lagodiuk <lagodykk@mail.ru>
2025-05-23 10:28:51 +0300

    Создан проект MVC

---
+++ src/Vikinger/Vikinger.Web/Program.cs
@@ -0,0 +1,35 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog(x => x.WriteTo.Console());
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.UseSerilogRequestLogging();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
